a4e7b935e7f5661ac07968eaf20e9cfc4836bae9
jimrom66 <188324624+jimrom66@users.noreply.github.com>
2024-12-19 17:59:26 +0200

    next time: make the flex basis responsive and go on with button and hovers

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ const divSize = Math.min(containerWidth, containerHeight) / Math.sqrt(numberOfBo
 for(let i = 0; i<16; i++){
     let newDiv=document.createElement("div");
     newDiv.classList.add("my_div");
-    newDiv.style.width = `${divSize}px`;  // Set width of each box
-    newDiv.style.height = `${divSize}px`; // Set height of each box
+    // newDiv.style.width = `${divSize}px`;  // Set width of each box
+    // newDiv.style.height = `${divSize}px`; // Set height of each box
     newDiv.textContent= i + 1;
     container.appendChild(newDiv);
 }

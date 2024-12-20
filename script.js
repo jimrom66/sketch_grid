3775f6a24cc9b68cac6a65d0eabdad1093547322
jimrom66 <188324624+jimrom66@users.noreply.github.com>
2024-12-20 14:47:42 +0200

    made all requirements and next time extrasss

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
 const container= document.querySelector(".cont_grid");
+const userInput=document.getElementById("quantity");
+const button=document.getElementById("submit_btn");
+let numberOfBoxes = 0
 
-let numberOfBoxes = 16
-const divSize = 800 / Math.sqrt(numberOfBoxes);
-const divBasis=Math.sqrt(numberOfBoxes)/numberOfBoxes;
+button.addEventListener("click",function(){
+    reset();
+    const input=userInput.value;
+    numberOfBoxes=input**2;
+    gridsketc(numberOfBoxes);
+})
 
-for(let i = 0; i<numberOfBoxes; i++){
+
+
+function gridsketc(numberOfBoxes){
+    const divSize = 800 / Math.sqrt(numberOfBoxes);
+    for(let i = 0; i<numberOfBoxes; i++){
     let newDiv=document.createElement("div");
     newDiv.classList.add("my_div");
     newDiv.style.width = `${divSize}px`;  // Set width of each box
     newDiv.textContent= i + 1;
     container.appendChild(newDiv);
+    newDiv.addEventListener("mouseenter",function(){
+        newDiv.style.backgroundColor="purple";
+    })
+}}
+
+function reset(){
+container.innerHTML=""
 }
+
     
     
